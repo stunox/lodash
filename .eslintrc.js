0b80fa58b08e9e9a364eaeafeeebac98514e2e25
Noctaz <nlbnn3@gmail.com>
2023-12-27 17:32:35 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    root: true,
+    extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    // extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    plugins: ['prettier', 'unused-imports'],
+    rules: {
+        'prettier/prettier': ['warn', require('./.prettierrc')],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        semi: ['error', 'always'],
+        'unused-imports/no-unused-imports': 2,
+        'unused-imports/no-unused-vars': 1,
+        'lines-between-class-members': [
+            'error',
+            'always',
+            {
+                exceptAfterSingleLine: true,
+            },
+        ],
+        'padding-line-between-statements': [
+            'error',
+            {
+                blankLine: 'always',
+                prev: ['const', 'let', 'var'],
+                next: '*',
+            },
+            {
+                blankLine: 'always',
+                prev: ['*'],
+                next: ['const', 'let', 'var'],
+            },
+            {
+                blankLine: 'never',
+                prev: ['const', 'let', 'var'],
+                next: ['const', 'let', 'var'],
+            },
+            {
+                blankLine: 'always',
+                prev: ['block-like'],
+                next: ['*'],
+            },
+            {
+                blankLine: 'always',
+                prev: ['*'],
+                next: ['block-like'],
+            },
+            {
+                blankLine: 'always',
+                prev: 'directive',
+                next: '*',
+            },
+            {
+                blankLine: 'never',
+                prev: 'directive',
+                next: 'directive',
+            },
+        ],
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    importOrder: ['<THIRD_PARTY_MODULES>', '@/(.*)', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    endOfLine: 'auto',
+};
